97a86d06985cf45d77ca2cc67074cd9a27f029d8
DevSourcerer04 <builder4107@gmail.com>
2025-07-24 22:00:29 +0530

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+
 export const Home = () => {
-    return <div className="min-h-screen *:backdrop:g-background text-foreground overfolw-x-hidden">
+    return(
+        <div className="min-h-screen *:backdrop:g-background text-foreground overfolw-x-hidden">
 
         {/* Theme Toggle */}
         <ThemeToggle />
         {/* Background Effects */}
-
+        <StarBackground />
         {/* Navbar */}
 
         {/* Main Content */}
 
         {/* Footer */}
     </div>
+    );
 };
