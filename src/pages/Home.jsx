f6573c384fb139cbee2b8787474997a841b0f7bf
DevSourcerer04 <builder4107@gmail.com>
2025-07-24 21:14:27 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+    return <div className="min-h-screen *:backdrop:g-background text-foreground overfolw-x-hidden">
+
+        {/* Theme Toggle */}
+        <ThemeToggle />
+        {/* Background Effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+    </div>
+};
